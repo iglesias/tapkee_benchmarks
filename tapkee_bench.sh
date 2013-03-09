090616b0cdefefc9f8497942015e91fbeaed6295
iglesias <fernando.iglesiasg@gmail.com>
2013-03-09 12:07:42 +0100

    rename klle to lle in tapkee benchmark script

--- tapkee_bench.sh
+++ tapkee_bench.sh
@@ -1,14 +1,14 @@
 echo ====LLE====
 echo "Swissroll 5000"
-./tapkee/tapkee -i data/swissroll5000.dat -o /dev/null -m klle -k 20 --benchmark
+./tapkee/tapkee -i data/swissroll5000.dat -o /dev/null -m lle -k 20 --benchmark
 echo "Swissroll 10000"
-./tapkee/tapkee -i data/swissroll10000.dat -o /dev/null -m klle -k 20 --benchmark
+./tapkee/tapkee -i data/swissroll10000.dat -o /dev/null -m lle -k 20 --benchmark
 echo "MNIST 2000"
-./tapkee/tapkee -i data/mnist2000.dat -o /dev/null -m klle -k 20 --benchmark
+./tapkee/tapkee -i data/mnist2000.dat -o /dev/null -m lle -k 20 --benchmark
 echo "MNIST 4000"
-./tapkee/tapkee -i data/mnist4000.dat -o /dev/null -m klle -k 20 --benchmark
+./tapkee/tapkee -i data/mnist4000.dat -o /dev/null -m lle -k 20 --benchmark
 echo "MIT-CBCL"
-./tapkee/tapkee -i data/cbcl.dat -o /dev/null -m klle -k 20 --benchmark
+./tapkee/tapkee -i data/cbcl.dat -o /dev/null -m lle -k 20 --benchmark
 
 echo ====Isomap====
 echo "Swissroll 5000"
